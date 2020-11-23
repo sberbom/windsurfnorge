7171479dac99af9dde5c4a0158f40a52e0a8f786
sberbom <sberbom@gmail.com>
2020-11-23 16:16:48 +0100

    added small images

--- src/components/cardList.js
+++ src/components/cardList.js
@@ -7,14 +7,22 @@ function CardList({spots}) {
     return(
         <div className="cardList-container">
             <div className="cardList">
-                {spots.map((spot, i) => 
-                    <Card
+                {spots.map((spot, i) => {
+                    let image = "https://fiskesnakk.files.wordpress.com/2010/09/img_8242-3.jpg"
+                    if(spot.smallImages && spot.smallImages[0]){
+                        image = spot.smallImages[0]
+                    }
+                    else if(spot.images && spot.images[0]) {
+                        image = spot.images[0]
+                    }
+                    return(
+                        <Card
                         key={i}
                         title={spot.name}
                         text={spot.about}
                         button="Se mer"
-                        image={spot.images && spot.images[0] ? spot.images[0] : "https://fiskesnakk.files.wordpress.com/2010/09/img_8242-3.jpg"}
-                    />
+                        image={image}
+                    />)}
                 )}
             </div>
         </div>

b05c28e025b4b03bdc92f233acc664c69aeb6c61
sberbom <sberbom@gmail.com>
2020-12-21 21:31:09 +0100

    updated footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,6 +4,24 @@ import '../styles/footer.css'
 function Footer(props) {
     return(
         <div className="footer-container">
+            <div className="footer-left">
+                <p>
+                    Windsurf Norge er et nettsted laget for å lære webutvikling. Nettsted vedlikeholdes etter ønske. 
+                    Dersom du har tilbakemeldinger eller ønsker å bidra kan du ta kontakt på sberbom@gmail.com
+                    Håper du får nytte av nettstedet!
+                </p>
+            </div>
+            <div className="footer-center">
+                Copyright - Windsurf Norge
+            </div>
+            <div className="footer-right">
+                <p>
+                    Kontakt: <br/>
+                    Sigmund Berbom <br/>
+                    <a href="https://sberbom.com">sberbom.com</a>
+                </p>
+
+            </div>
         </div>
     )
 }

ec1665f207da615036f51a95ac9dc6d8b2ea338b
sberbom <sberbom@gmail.com>
2021-01-10 18:43:44 +0100

    imporved depedency array image uploade

--- src/components/imageUploade.js
+++ src/components/imageUploade.js
@@ -13,100 +13,83 @@ const ImageUploade = ({ bigImageAsUrl, smallImageAsUrl, setSmallImageAsUrl, setB
     const [isUploading, setIsUploading] = useState(false);
     const [isUploadeCompleted, setIsUploadeCompleted] = useState(false);
     const [isError, setIsError] = useState(false);
-    const imageAsFile = [];
-    const bigImageAsFile = [];
-    const smallImageAsFile = [];
-    const bigImageName = [];
-    const smallImageName = [];
 
     const onDrop = useCallback( async (files) => {
-        for(let i = 0; i< files.length; i++) {
-            const image = files[i]
-            const compressedFileBig = await imageCompression(image, optionsBigImage);
-            const compressedFileSmall = await imageCompression(image, optionsSmallImage);
-            const randint = getRandomInt()
-            bigImageName.push(spotName + randint + i + 'big.jpg')
-            smallImageName.push(spotName + randint + 'small.jpg')
-            bigImageAsFile.push(compressedFileBig)
-            smallImageAsFile.push(compressedFileSmall)
-            imageAsFile.push(image)
+        const optionsBigImage = {
+            maxSizeMB: 5, 
+            maxWidthOrHeight: 1920,
+            useWebWorker: true
         }
-        handleFirebaseUploade()    
-      }, [])
-
-
-    const optionsBigImage = {
-        maxSizeMB: 5, 
-        maxWidthOrHeight: 1920,
-        useWebWorker: true
-    }
-
-    const optionsSmallImage = {
-        maxSizeMB: 2, 
-        maxWidthOrHeight: 286,
-        useWebWorker: true
-    }
-
-    const handleImageAsFile = async (event) => {
-        const files = event.target.files
-        for(let i = 0; i< files.length; i++) {
-            const image = files[i]
-            const compressedFileBig = await imageCompression(image, optionsBigImage);
-            const compressedFileSmall = await imageCompression(image, optionsSmallImage);
-            const randint = getRandomInt()
-            bigImageName.push(spotName + randint + i + 'big.jpg')
-            smallImageName.push(spotName + randint + 'small.jpg')
-            bigImageAsFile.push(compressedFileBig)
-            smallImageAsFile.push(compressedFileSmall)
-            imageAsFile.push(image)
+    
+        const optionsSmallImage = {
+            maxSizeMB: 2, 
+            maxWidthOrHeight: 286,
+            useWebWorker: true
         }
-        handleFirebaseUploade()    
-    } 
 
-    const handleFirebaseUploade = async () => {
-        for(let image in imageAsFile) {
-            if(image === '') {
-                console.error(`not an image, the image file is a ${typeof(imageAsFile)}`)
-                setIsError(true)
-                return;
-            }
-        }
-        setIsError(false)
-        setIsUploadeCompleted(false)
-        console.log('start of uploade')
-        setIsUploading(true);
-        
-        let newBigImageUrl = []
-        let newSmallImageUrl = []
-        const numberOfImages = bigImageName.length
-
-        for(let i = 0; i < numberOfImages; i++){
-
-            try{
-                await storage.ref(`/images/${bigImageName[i]}`).put(bigImageAsFile[i])
-                let fireBaseUrl = await storage.ref('images').child(bigImageName[i]).getDownloadURL()
-                newBigImageUrl.push(fireBaseUrl)
-                if(newBigImageUrl.length === numberOfImages) {
-                    setBigImageAsUrl(bigImageAsUrl.concat(newBigImageUrl))
-                    console.log("big completed")
-                    
+        const handleFirebaseUploade = async (imageAsFile, bigImageAsFile, smallImageAsFile, bigImageName, smallImageName) => {
+            for(let image in imageAsFile) {
+                if(image === '') {
+                    console.error(`not an image, the image file is a ${typeof(imageAsFile)}`)
+                    setIsError(true)
+                    return;
                 }
-            
-                await storage.ref(`/images/${smallImageName[i]}`).put(smallImageAsFile[i])
-                fireBaseUrl = await storage.ref('images').child(smallImageName[i]).getDownloadURL()
-                newSmallImageUrl.push(fireBaseUrl)
-                if(newSmallImageUrl.length === numberOfImages) {
-                    setSmallImageAsUrl(smallImageAsUrl.concat(newSmallImageUrl))
-                    console.log("Small completed")
-                    setIsUploadeCompleted(true);
-                    setIsUploading(false);
-                } 
             }
-            catch(error){
-                console.error("Could not uploade image", error)
+            setIsError(false)
+            setIsUploadeCompleted(false)
+            console.log('start of uploade')
+            setIsUploading(true);
+            
+            let newBigImageUrl = []
+            let newSmallImageUrl = []
+            const numberOfImages = bigImageName.length
+    
+            for(let i = 0; i < numberOfImages; i++){
+    
+                try{
+                    await storage.ref(`/images/${bigImageName[i]}`).put(bigImageAsFile[i])
+                    let fireBaseUrl = await storage.ref('images').child(bigImageName[i]).getDownloadURL()
+                    newBigImageUrl.push(fireBaseUrl)
+                    if(newBigImageUrl.length === numberOfImages) {
+                        setBigImageAsUrl(bigImageAsUrl.concat(newBigImageUrl))
+                        console.log("big completed")
+                        
+                    }
+                
+                    await storage.ref(`/images/${smallImageName[i]}`).put(smallImageAsFile[i])
+                    fireBaseUrl = await storage.ref('images').child(smallImageName[i]).getDownloadURL()
+                    newSmallImageUrl.push(fireBaseUrl)
+                    if(newSmallImageUrl.length === numberOfImages) {
+                        setSmallImageAsUrl(smallImageAsUrl.concat(newSmallImageUrl))
+                        console.log("Small completed")
+                        setIsUploadeCompleted(true);
+                        setIsUploading(false);
+                    } 
+                }
+                catch(error){
+                    console.error("Could not uploade image", error)
+                }
             }
         }
-    }
+        
+        const imageAsFile = []
+        const bigImageAsFile = []
+        const smallImageAsFile = []
+        const bigImageName = []
+        const smallImageName = []
+        for(let i = 0; i< files.length; i++) {
+            const image = files[i]
+            const compressedFileBig = await imageCompression(image, optionsBigImage);
+            const compressedFileSmall = await imageCompression(image, optionsSmallImage);
+            const randint = getRandomInt()
+            bigImageName.push(spotName + randint + i + 'big.jpg')
+            smallImageName.push(spotName + randint + 'small.jpg')
+            bigImageAsFile.push(compressedFileBig)
+            smallImageAsFile.push(compressedFileSmall)
+            imageAsFile.push(image)
+        }
+        handleFirebaseUploade(imageAsFile, bigImageAsFile, smallImageAsFile, bigImageName, smallImageName)    
+      }, [spotName, bigImageAsUrl, setBigImageAsUrl, smallImageAsUrl, setSmallImageAsUrl])
 
     const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
     
